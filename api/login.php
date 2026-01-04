8df5f6e3d3445e607166e57cee25f07db7bab56f
Tom <57322153+CyZeTLC@users.noreply.github.com>
2026-01-04 20:47:26 +0100

    fixed login.php & updated sql-query

--- api/login.php
+++ api/login.php
@@ -2,32 +2,25 @@
 require 'server.php';
 header("Content-Type: application/json");
 
-$dev = true;
+$dev = false;
 
 if ($dev) {
     $token = bin2hex(random_bytes(16));
     echo json_encode(["success" => true, "token" => $token]);
 } else {
-    $host = 'localhost';
-    $db   = 'deine_datenbank';
-    $user = 'root';
-    $pass = '';
-
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-
     $input = json_decode(file_get_contents('php://input'), true);
     $username = $input['username'];
     $password = $input['password'];
 
-    $stmt = $pdo->prepare("SELECT id, password_hash FROM users WHERE username = ?");
+    $stmt = $pdo->prepare("SELECT MITARBEITER_ID, PASSWORT FROM MITARBEITER WHERE `login` = ?");
     $stmt->execute([$username]);
     $user = $stmt->fetch();
 
-    if ($user && password_verify($password, $user['password_hash'])) {
+    if ($user && password_verify($password, $user['PASSWORT'])) {
         $token = bin2hex(random_bytes(16));
         echo json_encode(["success" => true, "token" => $token]);
     } else {
-        http_response_code(401);
+        //http_response_code(401);
         echo json_encode(["success" => false, "message" => "Login fehlgeschlagen"]);
     }
 }
