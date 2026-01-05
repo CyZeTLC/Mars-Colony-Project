17fbfa5d89702015a3dea53c5fc0fd36a4075b98
Tom <57322153+CyZeTLC@users.noreply.github.com>
2026-01-05 18:52:37 +0100

    Added query to view all citizens & if they work there

---
+++ sql/getAllCitizens.sql
@@ -0,0 +1,18 @@
+SELECT
+    b.bewohner_id,
+    b.vorname,
+    b.nachname,
+    a.stra_e,
+    a.hausnummer,
+    s.stadt_name,
+    CASE 
+        WHEN m.bewohner_id IS NOT NULL THEN 'Ja' 
+        ELSE 'Nein' 
+    END AS ist_mitarbeiter
+FROM BEWOHNER b
+INNER JOIN MITARBEITER m 
+    ON b.bewohner_id = m.bewohner_id
+INNER JOIN ADRESSE a 
+    ON b.adresse_id = a.adresse_id
+INNER JOIN STADT s 
+    ON a.stadt_id = s.stadt_id;
