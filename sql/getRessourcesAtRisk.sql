b9a8a23854ded31170ee2298dc90373ef50c50d5
CyZeTLC <tom@cyzetlc.de>
2026-01-05 13:37:39 +0100

    Fixed columns names for getRessourcesAtRisk.sql

--- sql/getRessourcesAtRisk.sql
+++ sql/getRessourcesAtRisk.sql
@@ -1,8 +1,8 @@
 SELECT r.ressource_id,
-    r.name,
+    r.r_typ as ressource_typ,
     r.ablaufdatum,
     ig.lager_id
-FROM Ressource r
-    JOIN ist_gelagert_in ig ON r.ressource_id = ig.ressource_id
+FROM RESSOURCE r
+    JOIN IST_GELAGERT_IN ig ON r.ressource_id = ig.ressource_id
 WHERE r.ablaufdatum BETWEEN CURRENT_DATE AND DATE_ADD(CURRENT_DATE, INTERVAL 30 DAY)
 ORDER BY r.ablaufdatum ASC;
