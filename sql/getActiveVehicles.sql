751c02251842fcf253626a3e160eaacdc9aeb87f
Tom <57322153+CyZeTLC@users.noreply.github.com>
2026-01-05 21:21:53 +0100

    changed sql query for active vehicles to also display the total & inactive amount

--- sql/getActiveVehicles.sql
+++ sql/getActiveVehicles.sql
@@ -1,3 +1,5 @@
-SELECT COUNT(*) AS vehicle_count
-FROM vehicles
-WHERE status = "active";
+SELECT 
+    SUM(CASE WHEN `F_STATUS` = 'im Einsatz' THEN 1 ELSE 0 END) AS active_vehicles_count,
+    SUM(CASE WHEN `F_STATUS` != 'im Einsatz' THEN 1 ELSE 0 END) AS inactive_vehicles_count,
+    COUNT(*) AS total_vehicles_count
+FROM FAHRZEUGE;
