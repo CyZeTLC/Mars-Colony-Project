904a1d11c7ac1faa326400670382e927487652fc
Tom <57322153+CyZeTLC@users.noreply.github.com>
2026-01-12 14:21:29 +0100

    changed query for active vehicles to also show orbital vehicles

--- sql/getActiveVehicles.sql
+++ sql/getActiveVehicles.sql
@@ -1,5 +1,9 @@
 SELECT 
-    SUM(CASE WHEN `F_STATUS` = 'im Einsatz' THEN 1 ELSE 0 END) AS active_vehicles_count,
-    SUM(CASE WHEN `F_STATUS` != 'im Einsatz' THEN 1 ELSE 0 END) AS inactive_vehicles_count,
+    SUM(CASE WHEN Status = 'im Einsatz' OR Status = 'im Flug' THEN 1 ELSE 0 END) AS active_vehicles_count,
+    SUM(CASE WHEN Status NOT IN ('im Einsatz', 'im Flug') THEN 1 ELSE 0 END) AS inactive_vehicles_count,
     COUNT(*) AS total_vehicles_count
-FROM FAHRZEUGE;
+FROM (
+    SELECT F_STATUS AS Status FROM FAHRZEUGE
+    UNION ALL
+    SELECT RF_STATUS AS Status FROM RAUMFAHRZEUG
+) combined_assets;
